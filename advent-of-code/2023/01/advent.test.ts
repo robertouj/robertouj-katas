2a099071b7496005f0ce174464c04d3b728e63f9
Roberto Ubieto Juan <robertouj@gmail.com>
2023-12-24 23:25:19 +0100

    Advent of code 2023 - 01

--- advent-of-code/2023/01/advent.test.ts
+++ advent-of-code/2023/01/advent.test.ts
@@ -1,24 +1,70 @@
 import { describe, it, expect } from 'vitest'
-import { combineFirstLastDigit, sumLinesNumbers } from './advent'
+import {
+  extractFirstAndLastDigit,
+  extractFirstAndLastDigitOrWord,
+  sumLinesNumbers,
+  sumLinesNumbersDigitOrWord,
+} from './advent'
 import fs from 'node:fs'
 
 describe('Advent test suite 2023 - 01', () => {
-  it.each([
-    { line: '1abc2', number: 12 },
-    { line: 'pqr3stu8vwx', number: 38 },
-    { line: 'a1b2c3d4e5f', number: 15 },
-    { line: 'treb7uchet', number: 77 },
-  ])('parses number from lines', ({ number, line }) => {
-    expect(combineFirstLastDigit(line)).toBe(number)
+  describe('Part One', () => {
+    it('get first and last digits from a string', () => {
+      const line = '1aba2ca7'
+
+      expect(17).toEqual(extractFirstAndLastDigit(line))
+    })
+
+    it.each([
+      { line: '1abc2', number: 12 },
+      { line: 'pqr3stu8vwx', number: 38 },
+      { line: 'a1b2c3d4e5f', number: 15 },
+      { line: 'treb7uchet', number: 77 },
+    ])('parses number from lines', ({ number, line }) => {
+      expect(extractFirstAndLastDigit(line)).toBe(number)
+    })
+
+    it('parses all lines and sums it up', () => {
+      const lines = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
+      expect(sumLinesNumbers(lines)).toBe(142)
+    })
+
+    it('parses all lines in a file', () => {
+      const lines = fs.readFileSync('./advent-of-code/2023/01/input.txt').toString().split('\n')
+      console.log(sumLinesNumbers(lines))
+    })
   })
 
-  it('parses all lines and sums it up', () => {
-    const lines = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
-    expect(sumLinesNumbers(lines)).toBe(142)
-  })
+  describe('Part Two', () => {
+    it.each([
+      { line: 'two1nine', number: 29 },
+      { line: 'eightwothree', number: 83 },
+      { line: 'abcone2threexyz', number: 13 },
+      { line: 'xtwone3four', number: 24 },
+      { line: '4nineeightseven2', number: 42 },
+      { line: 'zoneight234', number: 14 },
+      { line: '7pqrstsixteen', number: 76 },
+      { line: 'eighthree', number: 83 },
+    ])('parses number from lines', ({ number, line }) => {
+      expect(extractFirstAndLastDigitOrWord(line)).toBe(number)
+    })
 
-  it('parses all lines in a file', () => {
-    const lines = fs.readFileSync("./advent-of-code/2023/01/input.txt").toString().split("\n")
-    console.log(sumLinesNumbers(lines))
+    it('parses all lines and sums it up', () => {
+      const lines = [
+        'two1nine',
+        'eightwothree',
+        'abcone2threexyz',
+        'xtwone3four',
+        '4nineeightseven2',
+        'zoneight234',
+        '7pqrstsixteen',
+      ]
+      expect(sumLinesNumbersDigitOrWord(lines)).toBe(281)
+    })
+
+    it('parses all lines in a file', () => {
+      const lines = fs.readFileSync('./advent-of-code/2023/01/input.txt').toString().split('\n')
+      console.log(sumLinesNumbersDigitOrWord(lines))
+    })
   })
 })
--- advent-of-code/2023/01/advent.ts
+++ advent-of-code/2023/01/advent.ts
@@ -1,19 +1,64 @@
 export const combineFirstLastDigit = (line: string): number => {
-  const firstDigit = extractFirstDigit(line);
-  const lastDigit = extractFirstDigit(reverseString(line));
+  return Number(line)
+}
 
-  return Number(`${firstDigit}${lastDigit}`);
+export const extractFirstAndLastDigit = (line: string): number => {
+  const digits = []
+
+  for (const char of line) {
+    const digit = char.match(/\d/)
+    if (digit) {
+      digits.push(Number(digit[0]))
+    }
+  }
+
+  const firstDigit = digits[0]
+  const lastDigit = digits.slice(-1)[0]
+  return Number(`${firstDigit}${lastDigit}`) || 0
 }
 
 export const sumLinesNumbers = (lines: string[]): number => {
-  return lines.reduce((acc, line) => acc + combineFirstLastDigit(line), 0);
+  let sum = 0
+  for (const line of lines) {
+    sum += extractFirstAndLastDigit(line)
+  }
+  return sum
 }
 
-const extractFirstDigit = (line: string): string => {
-  return line.split("").find((char) => !isNaN(parseInt(char, 10))) ?? "";
+export const extractFirstAndLastDigitOrWord = (line: string): number => {
+  const digits = []
+
+  for (let index = 0; index < line.length; index++) {
+    const char = line[index]
+    const isDigit = char.match(/\d/)
+
+    if (isDigit) {
+      digits.push(Number(isDigit[0]))
+      continue
+    }
+
+    const word = line.substring(index)
+    const isNumberAsWord = word.match(/one|two|three|four|five|six|seven|eight|nine/)
+    const numbersAsWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    if (isNumberAsWord && isNumberAsWord?.index === 0) {
+      // add the number to the digits array
+      digits.push(numbersAsWords.indexOf(isNumberAsWord[0]) + 1)
+      // move the pointer forward to check the next char after the word
+      index += isNumberAsWord?.index || 0
+    }
+  }
+
+  const firstDigit = digits[0]
+  const lastDigit = digits.slice(-1)[0]
+
+  return Number(`${firstDigit}${lastDigit}`) || 0
 }
 
-const reverseString = (line: string): string => {
-  return line.split("").reverse().join("");
+export const sumLinesNumbersDigitOrWord = (lines: string[]): number => {
+  let sum = 0
+  for (const line of lines) {
+    sum += extractFirstAndLastDigitOrWord(line)
+  }
+  return sum
 }
-
